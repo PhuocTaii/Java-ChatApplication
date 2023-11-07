83b597d4b7d553e5bbcac5ef36b39ffd84ef6872
PhuocTaii <tai.ngo1773@gmail.com>
2023-11-07 15:23:06 +0700

    organized folders, and files

---
+++ Admin/src/main/java/com/btv/Admin/Admin.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.btv.Admin;
+
+/**
+ *
+ * @author taing
+ */
+public class Admin {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ User/src/main/java/com/btv/User/ChatClient.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.btv.User;
+
+/**
+ *
+ * @author Vo Quoc Binh
+ */
+public class ChatClient {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
+
