477291df08baa7238636b6cb40b7ca1fb8f53f0d
binhvo292003 <95414345+binhvo292003@users.noreply.github.com>
2023-11-30 00:15:09 +0700

    Add User Search GUI

--- User/src/main/java/com/btv/User/MainApp.java
+++ User/src/main/java/com/btv/User/MainApp.java
@@ -1,40 +1,45 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
-
 package com.btv.User;
+
 import com.btv.User.components.Layout;
 import com.btv.User.gui.Login;
+import com.btv.User.gui.Search;
 import com.btv.User.gui.SignUp;
 import javax.swing.SwingUtilities;
 
 import com.formdev.flatlaf.FlatLightLaf;
 import com.btv.User.helper.LoginListener;
 import com.btv.User.helper.SignUpListener;
+import javax.swing.JFrame;
+import javax.swing.WindowConstants;
 
 /**
  *
  * @author taing
  */
 public class MainApp {
+
     private Login logInForm;
     private SignUp signUpForm;
     private Layout mainLayout;
 
     public MainApp() {
         mainLayout = new Layout();
-        
+
         logInForm = new Login(new LoginListener() {
             @Override
             public void onSignUpLinkClicked() {
                 logInForm.setVisible(false);
                 signUpForm.setVisible(true);
             }
+
             @Override
             public void onLoginButtonClicked() {
                 logInForm.setVisible(false);
                 signUpForm.setVisible(false);
-                
+
                 mainLayout.setVisible(true);
                 mainLayout.setTitle("ChatChat");
             }
@@ -46,18 +51,24 @@ public class MainApp {
                 signUpForm.setVisible(false);
             }
         });
-        
+
         logInForm.setVisible(true);
         signUpForm.setVisible(false);
     }
 
-    public static void main(String args[]) {           
+    public static void main(String args[]) {
         FlatLightLaf.setup();
         /* Create and display the form */
         SwingUtilities.invokeLater(new Runnable() {
             @Override
             public void run() {
-                new MainApp();
+//                new MainApp();
+                JFrame frame = new JFrame();
+                frame.add(new Search());
+                frame.setSize(1080, 768);
+                frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+                frame.setVisible(true);
             }
         });
     }
