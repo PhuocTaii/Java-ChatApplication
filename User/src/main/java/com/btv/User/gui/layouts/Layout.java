8bdc76987d8e6f62b5b0f3371c3d319df0ada37b
Van Tran <thanhvan26823@gmail.com>
2023-11-30 10:13:49 +0700

    re-structure folder

--- User/src/main/java/com/btv/User/gui/layouts/Layout.java
+++ User/src/main/java/com/btv/User/gui/layouts/Layout.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
  */
-package com.btv.User.components;
+package com.btv.User.gui.layouts;
 
 import com.btv.User.gui.Chat;
 import com.btv.User.gui.Search;
